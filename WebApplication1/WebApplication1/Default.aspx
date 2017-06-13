4dd6b5a1b2714462520025f8a50341ab12a25971
odyyudah <odyyudah@gmail.com>
2017-06-13 16:05:10 +0700

    initial commit

---
+++ WebApplication1/WebApplication1/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1.Default" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=12.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <asp:Button ID="Button1" runat="server" Text="Save to MSExcel format" OnClick="Button1_Click" />
+        <rsweb:ReportViewer ID="ReportViewer1" runat="server" Font-Names="Verdana" Font-Size="8pt" WaitMessageFont-Names="Verdana" WaitMessageFont-Size="14pt" Width="800px">
+            <LocalReport ReportPath="Northwind.rdlc">
+            </LocalReport>
+        </rsweb:ReportViewer>
+    </div>
+        <asp:ObjectDataSource ID="ObjectDataSource1" runat="server" OldValuesParameterFormatString="original_{0}" SelectMethod="GetData" TypeName="WebApplication1.dsNorthwindTableAdapters.NorthwindTableAdapter"></asp:ObjectDataSource>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                dsNorthwind ds = new dsNorthwind();
+                dsNorthwindTableAdapters.NorthwindTableAdapter da = new dsNorthwindTableAdapters.NorthwindTableAdapter();
+                da.Fill(ds.Northwind);
+                var rptDataSource = new ReportDataSource("dsNorthwind", ds.Northwind.AsEnumerable());
+                rptDataSource.DataSourceId = "ObjectDataSource1";
+                rptDataSource.Name = "dsNorthwind";
+                ReportViewer1.LocalReport.DataSources.Add(rptDataSource);
+                ReportViewer1.DataBind();
+                ReportViewer1.LocalReport.Refresh();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Warning[] warnings;
+            string[] streamids;
+            string mimeType;
+            string encoding;
+            string extension;
+            string filename;
+
+            byte[] bytes = ReportViewer1.LocalReport.Render(
+               "EXCELOPENXML", null, out mimeType, out encoding,
+                out extension,
+               out streamids, out warnings);
+
+            filename = string.Format("{0}.{1}", "Northwind", "xlsx");
+            Response.ClearHeaders();
+            Response.Clear();
+            Response.AddHeader("Content-Disposition", "attachment;filename=" + filename);
+            Response.ContentType = mimeType;
+            Response.BinaryWrite(bytes);
+            Response.Flush();
+            Response.End();
+        }
+    }
+}
